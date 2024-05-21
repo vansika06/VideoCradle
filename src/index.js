fa44b9821f885a021b57c6e27b22f4457d4ad7f4
Vansika <vansikasinghania06@gmail.com>
2024-05-21 12:58:22 +0530

    connected to DB

---
+++ src/index.js
@@ -0,0 +1,35 @@
+//require('dotenv').config({path:'./env'}) -- this will work but syntax mismatch
+import dotenv from "dotenv"
+import mongoose, { connect } from "mongoose"
+import {DB_NAME} from "./constants.js"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+
+
+
+
+/*import express from "express"
+const app=express()
+/*function connectDB(){
+
+}
+connectDB()
+;(async()=>{
+    try{
+      await  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+      app.on("error",(error)=>{
+        console.log("ERR:",error);
+        throw error
+      })
+      app.listen(process.env.PORT,()=>{
+        console.log(`app is listening at:${process.env.PORT}`)
+      })
+    }
+    catch(e){
+        console.error("Error:",e);
+        throw e
+    }
+})()*/
